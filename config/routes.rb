5a1d585e10e8b356347e7d8c62eb0529f72545b5
Christian Ruiz <santacruz.crm@gmail.com>
2020-07-24 13:39:34 -0700

    Add Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+
+  devise_for :users, :path => 'u', :controllers => { registrations: 'registrations' }
+  resources :users, :only =>[:show, :index]
   resources :posts do
     resources :comments
   end
