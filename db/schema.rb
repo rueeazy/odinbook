e880d920e2e045fe7ca220f4426cdd48759fd967
Christian Ruiz <santacruz.crm@gmail.com>
2020-11-08 12:33:17 -0800

    Add bio feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_191747) do
+ActiveRecord::Schema.define(version: 2020_11_08_011020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2020_07_31_191747) do
     t.string "uid"
     t.string "name"
     t.text "image"
+    t.string "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["first_name"], name: "index_users_on_first_name"
     t.index ["last_name"], name: "index_users_on_last_name"
