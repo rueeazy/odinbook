e5005acc0b0578519478178cd61a3d74a3fcdc69
Christian Ruiz <santacruz.crm@gmail.com>
2020-11-09 12:01:58 -0800

    Navbar space fix & mailer disable

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,8 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
 
     def create
         super
-        if @user.persisted?
-            UserMailer.welcome_email(@user).deliver_now
+        if false
+            if @user.persisted?
+                UserMailer.welcome_email(@user).deliver_now
+            end
         end
     end
 
